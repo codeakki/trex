eb0dc3c3ba1f1749a1f5b094d46bad32ec7f8f5f
Akshay Sharma <akshaysharma@Akshays-MacBook-Air.local>
2022-11-18 17:07:47 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import helloRt from "./pages/helloRt";
+function App() {
+  return (
+    <div className="App">
+     <h1>The Run</h1>
+     <div className="screen-share">
+       <helloRt />
+      </div>
+    </div>
+  );
+}
+
+export default App;
